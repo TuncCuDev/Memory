5c2c7f1aa14081dbc1d9ee5a90bbbbe7610e5f2e
TuncCu <tuncay-cubukcu@hotmail.de>
2026-07-28 11:48:47 +0200

    cretaed setting page left side

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import './styles/style.scss'
 
-function init() {
-    const fieldRef = document.getElementById('field')
-    if(fieldRef) {
-        fieldRef.addEventListener("click", e =>{
-            const card = (e.target as HTMLElement ).closest(".card") as HTMLButtonElement;
-            if (card) {
-                card.classList.toggle("is-flipped")
-            }
-        })
-    }
+function makeSelectable(selector: string) {
+    const items = document.querySelectorAll(selector);
+
+    items.forEach(item => {
+        item.addEventListener("click", () => {
+            console.log(item.classList)
+            items.forEach(i => i.classList.remove("active"));
+            item.classList.add("active");
+            console.log(item.classList)
+        });
+    });
 }
+
+console.log(document.querySelectorAll(".main-section__theme").length);
+makeSelectable(".main-section__player");
+makeSelectable(".main-section__size");
+makeSelectable(".main-section__theme");
