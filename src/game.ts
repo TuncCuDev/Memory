f1a183a24bc6e9918a493dbf625d3e9c20d245b3
TuncCu <tuncay-cubukcu@hotmail.de>
2026-08-04 10:23:04 +0200

    open memory game cards

--- src/game.ts
+++ src/game.ts
@@ -1,35 +1,119 @@
 import './styles/style.scss'
 
+
+const CodeImages = [
+    "A (1).svg",
+    "A (2).svg",
+    "A (3).svg",
+    "A (4).svg",
+    "A (5).svg",
+    "A (6).svg",
+    "A (7).svg",
+    "A (8).svg",
+    "A (9).svg",
+    "A (10).svg",
+    "A (11).svg",
+    "A (12).svg",
+    "A (13).svg",
+    "A (14).svg",
+    "A (15).svg",
+    "A (16).svg",
+    "A (17).svg",
+    "A (18).svg",
+]
+
+const DAImages = [
+    "DA  (1).svg",
+    "DA  (2).svg",
+    "DA  (3).svg",
+    "DA  (4).svg",
+    "DA  (5).svg",
+    "DA  (6).svg",
+    "DA  (7).svg",
+    "DA  (8).svg",
+    "DA  (9).svg",
+    "DA  (10).svg",
+    "DA  (11).svg",
+    "DA  (12).svg",
+    "DA  (14).svg",
+    "DA  (15).svg",
+    "DA  (16).svg",
+    "DA  (17).svg",
+    "DA  (18).svg",
+]
+
 const theme = localStorage.getItem("theme");
 const player = localStorage.getItem("player");
 const size = localStorage.getItem("size");
 
-console.log(theme);
-console.log(player);
-console.log(size);
+let cardCount = 16;
 
-if (theme === "Code vibes theme") {
-    // Code-Vibes-Bilder verwenden
-} else if (theme === "DA Projects theme") {
-    // DA-Projects-Bilder verwenden
+getCardCount();
+
+let images = theme === "Code vibes theme" ? CodeImages : DAImages;
+
+const pairCount = cardCount / 2;
+const selectedImages = images.slice(0, pairCount);
+const cards = [...selectedImages, ...selectedImages];
+
+cards.sort(() => Math.random() - 0.5);
+
+
+function loadTheme() {
+    if (theme === "Code vibes theme") {
+        document.body.classList.add("theme-code");
+    } else if (theme === "DA Projects theme") {
+        document.body.classList.add("theme-da");
+    }
 }
 
-if (player === "Blue") {
-    // Blauer Spieler beginnt
-} else if (player === "Orange") {
-    // Orangener Spieler beginnt
+function setStartPlayer() {
+    if (player === "Blue") {
+        console.log("Blue beginnt");
+    } else {
+        console.log("Orange beginnt");
+    }
 }
 
-switch (size) {
-    case "16 cards":
-        // 16 Karten erstellen
-        break;
+function getCardCount() {
+    const board = document.querySelector(".game-board");
+    switch (size) {
+        case "16 cards":
+            cardCount = 16;
+            board?.classList.add("grid-4x4");
+            break;
 
-    case "24 cards":
-        // 24 Karten erstellen
-        break;
+        case "24 cards":
+            cardCount = 24;
+            board?.classList.add("grid-6x4");
+            break;
 
-    case "36 cards":
-        // 36 Karten erstellen
-        break;
+        case "36 cards":
+            cardCount = 36;
+            board?.classList.add("grid-6x6");
+            break;
+    }
 }
+
+function createBoard(cards: string[]) {
+    const board = document.querySelector(".game-board");
+
+    if (!board) return;
+
+    board.innerHTML = "";
+
+    cards.forEach(image => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.src = `../assets/${image}`;
+
+        card.appendChild(img);
+        board.appendChild(card);
+    });
+}
+
+loadTheme();
+setStartPlayer();
+createBoard(cards);
